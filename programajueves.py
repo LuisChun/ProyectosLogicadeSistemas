66360e9be20614daedf5038f069205187eaeed10
LuisChun <168605624+LuisChun@users.noreply.github.com>
2024-05-04 22:18:44 -0600

    Calculo de area de un rectangulo y para pedir el nombre, edad y que le tire el año de nacimiento

---
+++ programajueves.py
@@ -0,0 +1,49 @@
+#Calculo del area de un rectangulo.
+'''base = 0
+altura = 0
+base = input("Ingrese la base del rectangulo: ")
+base = int(base) #Convertir de string a variable entera (int)
+altura = input("Ingrese la altura del rectangulo: ")
+altura = int(altura) #Convertir de string a variable entera (int), para usar flotante es para que sea con numeros decimales.
+area = base*altura
+perimetro= 2 * base  + 2* altura
+print ("El area del rectangulo es: ", area)
+print("El perimetro del rectangulo es: ", perimetro)'''
+
+'''radio = 0
+PI= 3.14
+# pedimos el radio
+radio= input("Ingrese el radio del circulo: ")
+# Convertirmos en flotante
+radio=float(radio)
+# Calculamos el area
+area = PI*radio**2 
+# Calculamos el perimetro
+perimetro = 2*PI*radio
+#Mostramos los resultados
+print("El area del circulo es: ", area)
+print("El perimetro del circulo es: ", perimetro)'''
+
+'''#Crear las variables para los catetos
+cateto1=0
+cateto2 = 0
+#pedir los valores de los catetos
+cateto1=float(input("Ingrese el valor del cateto 1: "))
+cateto2=float(input("Ingrese el valor del cateto 2: "))
+# Calcular la hipotenusa
+hipotenusa= (cateto1**2+cateto2**2)**(1/2)
+#Mostrar el resultado
+print("La hipotenusa es: ",hipotenusa)'''
+
+# pedir nombre y edad para que le tire tambien en que año nacio teninedo la edad
+import datetime
+nombre = input("Cual es tu nombre: ")
+anoniac = int(input("Cual es tu edad: "))
+año = 2024 - anoniac
+print("Hola", nombre)
+print("Naciste en el año",año )
+
+now = datetime.now()
+print(now)
+
+
